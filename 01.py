7b9b02b3900c4f285de327cdebc3a21338a8edbf
Calixto!!! <103325008+Calixto0128@users.noreply.github.com>
2022-04-30 12:04:49 -0300

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,25 @@
+lista_Vazia = []
+print(f'Lista Vazia: {lista_Vazia}')
+print(f'Tipo da Lista: {type(lista_Vazia)}')
+
+
+Lista_Inteiros = [1, 4, 5, 7, 8, 9]
+print(f'Valores contados na lista: {Lista_Inteiros}')
+
+
+lista_tipos_diferentes = ['Brasil', 5, 'Titulos']
+print(F'{lista_tipos_diferentes} ')
+
+
+Nome = 'Infinity'
+Inteiro = 100
+Flutuante = 2.58
+Booleano = True
+lista_Varios_tipos = [Nome, Inteiro, Flutuante, Booleano]
+print(f'{lista_Varios_tipos}')
+
+Lista_Função = [10, 20, 30, 40, 50]
+def lista(Lista_Função):
+    print(f'Os valores são: {Lista_Função}')
+
+lista(Lista_Função)
