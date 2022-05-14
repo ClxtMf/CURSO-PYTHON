251aaf048a904d29a35febba3cf841b57803f19e
CalixtoTech <103325008+Calixto0128@users.noreply.github.com>
2022-05-14 10:55:57 -0300

    Update Encapsulamento.py

--- Encapsulamento.py
+++ Encapsulamento.py
@@ -24,3 +24,44 @@ class Pessoa:
 
 
 pessoa = Pessoa()
+
+"""""""""""""""""""""""""""""""""""""""""""""
+
+class Pessoa:
+    def __init__(self):
+        self.__nome = ''
+        self.__idade = 0
+        self.__cpf = ''
+    @property
+    def nome(self):
+        return self.__nome
+    @property
+    def idade(self):
+        return self.__idade
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @idade.setter
+    def idade(self, idade):
+        self.__idade = idade
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self.__cpf = cpf
+
+
+pessoa = Pessoa()
+
+
+pessoa.nome = input('Digite o seu nome: ')
+pessoa.idade = int(input('Digite a sua idade: '))
+pessoa.cpf = input('Digite o seu cpf: ')
+
+print(f'Nome: {pessoa.nome}')
+print(f'Idade: {pessoa.idade}')
+print(f'CPF: {pessoa.cpf}')
